4ba70edc4593773fd1ab0cd431fe72a37a1ea733
NatanCastro <castro.natan@outlook.com>
2023-05-13 17:38:24 -0300

    update game position on a list

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -2,11 +2,14 @@ package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.dto.ReplacementDTO;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +25,12 @@ public class GameListController {
     @Autowired
     private GameService gameService;
 
-    @RequestMapping(value = {"", "/"})
+    @GetMapping(value = {"", "/"})
     public List<GameListDTO> findAll() {
         return gameListService.findAll();
     }
 
-    @RequestMapping(value = "/{listId}")
+    @GetMapping(value = "/{listId}")
     public GameListDTO findById(@PathVariable Long listId) {
         return gameListService.findById(listId);
     }
@@ -35,5 +38,10 @@ public class GameListController {
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         return gameService.findByList(listId);
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,7 +2,10 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entites.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +16,36 @@ import java.util.List;
 public class GameListService {
 
     @Autowired
-    private GameListRepository repository;
+    private GameListRepository gameListRepository;
+
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
-        List<GameList> gameLists = repository.findAll();
+        List<GameList> gameLists = gameListRepository.findAll();
         return gameLists.stream().map(GameListDTO::new).toList();
     }
 
     @Transactional(readOnly = true)
     public GameListDTO findById(Long id) {
-        GameList gameList = repository.findById(id).isPresent() ? repository.findById(id).get() : null;
+        GameList gameList = gameListRepository
+          .findById(id).isPresent() ? gameListRepository.findById(id).get() : null;
         return gameList != null ? new GameListDTO(gameList) : null;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+      List<GameMinProjection> list = gameRepository.findByList(listId);
+
+      GameMinProjection movedItem = list.remove(sourceIndex);
+      list.add(destinationIndex, movedItem);
+
+      int min = Math.min(sourceIndex, destinationIndex);
+      int max = Math.max(sourceIndex, destinationIndex);
+
+      for (int i = min; min <= max; i++) {
+        gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+      }
     }
 }
